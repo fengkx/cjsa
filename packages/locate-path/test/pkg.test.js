940fd12e2768e4870428d62a7f1bea2bf6ffcc2b
fengkx <liangkx8237@gmail.com>
2022-01-21 03:30:07 +0800

    pkg: locate-path v7.0.0

---
+++ packages/locate-path/test/pkg.test.js
@@ -0,0 +1,9 @@
+const locatePath = require('..');
+
+describe('locate-path cjs', () => {
+    test('should be defined', () => {
+        expect(locatePath).toBeDefined();
+        expect(locatePath.locatePath).toBeDefined();
+        expect(locatePath.locatePathSync).toBeDefined();
+    })
+})
