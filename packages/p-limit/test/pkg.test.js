34d75cc6f78e1698a9f56f3fe7d09bd44a50fa19
fengkx <liangkx8237@gmail.com>
2022-01-21 03:29:08 +0800

    pkg: p-limit v4.0.0

---
+++ packages/p-limit/test/pkg.test.js
@@ -0,0 +1,8 @@
+const pLimit = require('..');
+
+describe('pLimit cjs', () => {
+    test('should be defined', () => {
+        expect(pLimit).toBeDefined();
+        expect(pLimit.default).toBeInstanceOf(Function)
+    })
+})
