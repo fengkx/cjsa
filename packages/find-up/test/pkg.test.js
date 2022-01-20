11c36366f4595ee3af2459d59b0a290cef0ecb50
fengkx <liangkx8237@gmail.com>
2022-01-21 03:33:08 +0800

    pkg: find-up v6.2.0

---
+++ packages/find-up/test/pkg.test.js
@@ -0,0 +1,13 @@
+const findup = require('..');
+
+describe('find-up cjs', () => {
+    test('should be defined', () => {
+        expect(findup).toBeDefined();
+        expect(findup.findUp).toBeInstanceOf(Function);
+        expect(findup.findUpMultiple).toBeInstanceOf(Function);
+        expect(findup.findUpMultipleSync).toBeInstanceOf(Function);
+        expect(findup.findUpSync).toBeInstanceOf(Function);
+        expect(findup.pathExists).toBeInstanceOf(Function);
+        expect(findup.pathExistsSync).toBeInstanceOf(Function);
+    })
+})
