f01475e35516781fb5db45044921547a1edeb157
fengkx <liangkx8237@gmail.com>
2022-01-21 03:27:49 +0800

    pkg: yocto-queue v1.0.0

---
+++ packages/yocto-queue/test/pkg.test.js
@@ -0,0 +1,8 @@
+const yoctoQueue = require('..');
+
+describe('yoctoQueue cjs', () => {
+    test('should be defined', () => {
+        expect(yoctoQueue).toBeDefined();
+        expect(yoctoQueue.default).toBeInstanceOf(Function)
+    })
+})
