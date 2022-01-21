df56c30f69b9b0e143d8ede0fbaa0da13c69bf31
fengkx <liangkx8237@gmail.com>
2022-01-21 14:53:57 +0800

    feat: plop generator

---
+++ internals/scripts/plopfile.js
@@ -0,0 +1,30 @@
+require('zx/globals');
+const {absPackagesDir, absModulesDir} = require('@tocjs/internals-shared/const');
+
+module.exports = function(plop) {
+    plop.setGenerator('package name', {
+		description: 'package name on npm',
+		prompts: [{
+			type: 'input',
+			name: 'name',
+			message: 'package name please'
+		}],
+		actions: [
+			{
+				type: 'add',
+				path: `${absPackagesDir}/{{name}}/build-cjs.js`,
+				templateFile: 'plop-templates/new-package/build-cjs.hbs'
+			},
+			{
+				type: 'add',
+				path: `${absPackagesDir}/{{name}}/package-patch.json`,
+				templateFile: 'plop-templates/new-package/package-patch.hbs'
+			},
+			{
+				type: 'add',
+				path: `${absPackagesDir}/{{name}}/test/pkg.test.js`,
+				templateFile: 'plop-templates/new-package/test/pkg.test.hbs'
+			}
+		]
+	});
+}
