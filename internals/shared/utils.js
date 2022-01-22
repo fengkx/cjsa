64d4e263e40990cfe120f928a83722d54512551f
fengkx <liangkx8237@gmail.com>
2022-01-23 00:44:55 +0800

    feat: getAllPackages filter by package.json

--- internals/shared/utils.js
+++ internals/shared/utils.js
@@ -1,8 +1,16 @@
-const { fs } = require("zx");
+require("zx/globals");
 const { absPackagesDir } = require("./const");
 
 exports.getAllPackages = async () => {
   const files = await await fs.readdir(absPackagesDir, { withFileTypes: true });
-  const packages = files.filter((f) => f.isDirectory()).map((d) => d.name);
+  const packages = files
+    .filter((f) => {
+      const isDir = f.isDirectory();
+      const hasPackageJson = fs.existsSync(
+        path.join(absPackagesDir, f.name, "package.json")
+      );
+      return isDir && hasPackageJson;
+    })
+    .map((d) => d.name);
   return packages;
 };
